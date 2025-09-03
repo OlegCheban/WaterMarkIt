83cfa24aded2c0142b5b09d6b7a5c3a8d42001e0
Mukesh <mukeshwarank2001@gmail.com>
2025-09-03 21:54:30 +0530

    Reordered bold() method for consistency in formatting

--- src/main/java/com/markit/api/builders/TextBasedWatermarkBuilder.java
+++ src/main/java/com/markit/api/builders/TextBasedWatermarkBuilder.java
@@ -12,34 +12,34 @@ import java.awt.*;
 */
 public interface TextBasedWatermarkBuilder<WatermarkBuilderType> {
 
-   /**
-    * The color of the text
-    *
-    * @param color The color for the text
-    * @see Color
-    */
-   TextBasedWatermarkBuilder<WatermarkBuilderType> color(Color color);
+  /**
+   * The color of the text
+   *
+   * @param color The color for the text
+   * @see Color
+   */
+  TextBasedWatermarkBuilder<WatermarkBuilderType> color(Color color);
 
-   /**
-    * The font of the text
-    *
-    * @param font The font for the text
-    * @see com.markit.api.Font
-    */
-   TextBasedWatermarkBuilder<WatermarkBuilderType> font(Font font);
+  /**
+   * The font of the text
+   *
+   * @param font The font for the text
+   * @see com.markit.api.Font
+   */
+  TextBasedWatermarkBuilder<WatermarkBuilderType> font(Font font);
 
-   /**
-    * Adds a trademark symbol to the text
-    */
-   TextBasedWatermarkBuilder<WatermarkBuilderType> addTrademark();
+  /**
+   * Makes the text bold
+   */
+  TextBasedWatermarkBuilder<WatermarkBuilderType> bold();
 
-   /**
-    * Makes the text bold
-    */
-   TextBasedWatermarkBuilder<WatermarkBuilderType> bold();
+  /**
+   * Adds a trademark symbol to the text
+   */
+  TextBasedWatermarkBuilder<WatermarkBuilderType> addTrademark();
 
-   /**
-    * Finish working with TextBasedWatermarkBuilder and back to the WatermarkBuilderType
-    */
-   WatermarkBuilderType end();
+  /**
+   * Finish working with TextBasedWatermarkBuilder and back to the WatermarkBuilderType
+   */
+  WatermarkBuilderType end();
 }
